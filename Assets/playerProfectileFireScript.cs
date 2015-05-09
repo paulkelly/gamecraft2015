44891714e3f84ad0a800f2f8ee621f92469a88e2
NinjaDanceMat <cathcart100@googlemail.com>
2015-05-09 12:19:05 +0100

    Preliminary Aim Script

---
+++ Assets/playerProfectileFireScript.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playerProfectileFireScript : MonoBehaviour {
+
+	public Vector2 aimAngle = new Vector2 ();
+	public bool fireTrue;
+	public GameObject projectile;
+	public Object currentProjectile;
+
+	// Use this for initialization
+	void Start () {
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		//Vector2 aimAngle = controllerInput;
+
+		//bool fireTrue = controllerInput;
+
+		if (fireTrue == true)
+		{
+
+			currentProjectile = Instantiate(projectile, new Vector3(transform.localPosition.x, transform.localPosition.y, transform.localPosition.z), Quaternion.identity);
+
+			fireTrue = false;
+
+		}
+	}
+}
